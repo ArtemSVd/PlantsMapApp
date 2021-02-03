6695a3f309fff1d6b43fd0e46c13a925882a3432
Artem <artem-vdovin@mail.ru>
2021-02-03 18:10:40 +0300

    Добавление таблицы для хранения сущностей Растения/Грибы

---
+++ liquibase/src/main/liquibase/change/create_tables/m_plant.sql
@@ -0,0 +1,34 @@
+create table if not exists m_plant
+(
+    id integer not null
+        constraint pk_plant
+            primary key,
+    name varchar(1000) not null,
+    description varchar(3000),
+    kingdom_type varchar(50) not null,
+    file_path varchar(500),
+    created_date timestamp,
+    user_id integer not null,
+    coordinate varchar(100)
+);
+
+create sequence seq_plant;
+
+comment on table m_plant is 'Растения/Грибы';
+
+comment on column m_plant.id is 'Идентификатор';
+
+comment on column m_plant.name is 'Название';
+
+comment on column m_plant.description is 'Описание';
+
+comment on column m_plant.kingdom_type is 'Тип царства, к которому относится сущность (грибы, растения)';
+
+comment on column m_plant.file_path is 'Путь к файлу в файловом хранилище';
+
+comment on column m_plant.created_date is 'Дата добавления сущности в базу';
+
+comment on column m_plant.user_id is 'Идентификатор пользователя, от которого пришла сущность';
+
+comment on column m_plant.coordinate is 'Географические координаты местонахождения сущности';
+
